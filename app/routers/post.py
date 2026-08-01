26d2418ac7b48cc3c5ca37b57a576ade742aa1d3
AlenPJogy <alenpjoji@gmail.com>
2026-08-01 17:24:32 +0530

    done

--- app/routers/post.py
+++ app/routers/post.py
@@ -24,6 +24,7 @@ async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(o
 
     posts = (db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
                .join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True)
+               .join(models.Comment, models.Comment.post_id == models.Post.id, isouter=True)
                .group_by(models.Post.id)
                .filter(models.Post.title.contains(search))
                .limit(limit)
