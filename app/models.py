ef7051dd8fbeff223eb15d221171c1c8542ea132
AlenPJogy <alenpjoji@gmail.com>
2026-08-01 18:26:23 +0530

    done

--- app/models.py
+++ app/models.py
@@ -35,6 +35,5 @@ class Comment(Base):
     comment=Column(String, nullable=False, default="no comments yet")
     post_id=Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
     user_id=Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    post=relationship("Post", backref="comments")
     user=relationship("User")
     post = relationship("Post", back_populates="comments")
